4a5162b25b3f470136da12b38185eb0880571c6c
Amer-N-Sulaiman <amersulaiman10@gmail.com>
2023-06-13 16:54:46 +0300

    Added User Auth Routes

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const login = (req, res)=>{
+    res.json('login')
+}
+
+const signup = (req, res)=>{
+    res.json('signup')
+}
+
+module.exports = {login, signup}
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const userRoutes = require('./routes/userRoutes')
 require('dotenv').config()
 
 const app = express()
+
+// routes
+app.use('/auth', userRoutes)
+
+
 
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
